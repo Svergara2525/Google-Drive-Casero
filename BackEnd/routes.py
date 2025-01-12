bd112951878320c27b2c827f428a6e55fb6734f5
sergio <sergioverdom@gmail.com>
2025-01-12 12:27:36 +0100

    subir archivos y avanzar/retroceder

--- BackEnd/routes.py
+++ BackEnd/routes.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from werkzeug.utils import secure_filename
 import os
 
 from flasgger import Swagger, swag_from
@@ -13,12 +14,10 @@ swagger = Swagger(app, config=swagger_config, template=swagger_template)
 dir_path = os.path.join(os.path.expanduser("~"), "Escritorio")
 
 
-directories = [ f.path for f in os.scandir(dir_path) if f.is_dir() ]
-
-
 @app.route('/conexion')
 @swag_from('Swagger/conexion.yml')
 def conexion():
+    directories = [ f.path for f in os.scandir(dir_path) if f.is_dir()]
     directory_names = [os.path.basename(d) for d in directories]
     return jsonify({"mensaje": directory_names})
 
@@ -26,13 +25,41 @@ def conexion():
 @app.route('/navegar/<string:directorio>')
 @swag_from('Swagger/navegar.yml')
 def navegar(directorio):
+    global dir_path
+    directories = [ f.path for f in os.scandir(dir_path) if f.is_dir()]
     directory_names = [os.path.basename(d) for d in directories]
     if (directorio in directory_names):
         subfolders = [ f.path for f in os.scandir(dir_path + '/' + directorio) if f.is_dir()]
+        dir_path = dir_path + '/' + directorio
+        app.config['UPLOAD_FOLDER'] = dir_path
     else:
         subfolders = "No existe el subdirectorio"
     return jsonify({"subcarpetas": subfolders})
 
 
+@app.route('/retroceder')
+@swag_from('Swagger/retroceder.yml')
+def retroceder():
+    global dir_path
+    dir_path = os.path.dirname(dir_path)
+    subfolders = [ f.path for f in os.scandir(dir_path) if f.is_dir()]
+    app.config['UPLOAD_FOLDER'] = dir_path
+    return jsonify({"subcarpetas": subfolders})
+
+
+
+@app.route('/subir_archivo', methods=['POST'])
+@swag_from('Swagger/subir_archivo.yml')
+def subir_archivo():
+    if 'file' not in request.files:
+        return jsonify({"mensaje": "No se ha enviado el archivo"}), 400
+    files = request.files.getlist('file')
+    if len(files) == 0:
+        return jsonify({"mensaje": "No se ha seleccionado el archivo"}), 404
+    for file in files:
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return jsonify({"mensaje": "Archivo subido"}), 200
+    
 if __name__ == '__main__':
     app.run(debug=True)
